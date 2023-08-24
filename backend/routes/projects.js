694eb15ae1f84bd2cb0890bece87e1f2e3d1115c
Makyla Scoullar <makylascoullar@Makylas-MacBook-Pro.local>
2023-08-25 11:47:02 +1200

    This adds the getProject, update and delete

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -3,11 +3,24 @@ const express = require ('express')
 const router = express.Router()
 
 //Import controllers
-const {getProjects,createProject } = require ('../controllers/projectController')
+const {
+    getProjects,
+    getProject,
+    createProject,
+    updateProject,
+    deleteProject
+} = require ('../controllers/projectController')
 
 router.get('/', getProjects)
 
+// GET a single workout
+router.get('/:id', getProject)
+
 router.post('/', createProject)
 
+// UPDATE a workout
+router.patch('/:id', updateProject)
+
+router.delete('/', deleteProject)
 
 module.exports = router;
