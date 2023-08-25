651d2feabd169a75acc8a36e21aaa4aca9522f79
ntoni596 <ntoni596@gmail.com>
2023-08-25 14:03:39 +1200

    Homepage styling and changes

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -6,7 +6,7 @@ import { useProjectContext } from "./hooks/useProjectContext";
 // import context ----------------------------------------------------------------
 
 // import components ----------------------------------------------------------------
-
+import headerImage from "./assets/header-background.png";
 // Landing image + header
 // h1 students portfolio
 // grid to display students
@@ -44,6 +44,7 @@ const Homepage = () => {
   return (
     <>
       <div className="header-image">
+        <img className="headerImg" src={headerImage} />
         <h2 className="header">
           Yoobee College of Creative Innovation is New Zealand’s largest
           specialist creative and technology college.
@@ -51,6 +52,7 @@ const Homepage = () => {
       </div>
 
       <div className="students-grid-container">
+        <h1 className="section-heading">Sudents Portfolio</h1>
         {projects.map((project) => (
           <div key={project.id} className="grid-item">
             <h3>{project.title}</h3>
