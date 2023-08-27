f5cdc7f54a23e1233e127169a0af06a105b5bbf3
Jon3sy <ethan.jonesshark@gmail.com>
2023-08-28 09:22:35 +1200

    jwt-token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
 
 const loginUser = async (req,res) => {
     res.json({mssg: 'login user'})
@@ -9,8 +14,11 @@ const signUpUser = async (req,res) => {
 
     try{
         const user = await User.signup(email,password)
+
+        const token = createToken(user._id)
          
-        res.status(200).json({email,password})
+        res.status(200).json({email, token})
+
     } catch (error) {
         res.status(400).json({error: error.message})
     }
