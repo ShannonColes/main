739b65c6f660a7e27a31d13ea98ce3ad19a275f7
Makyla Scoullar <makylascoullar@Makylas-MacBook-Pro.local>
2023-08-30 22:02:01 +1200

    update project navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,44 +6,52 @@ const Navbar = () => {
 
   return (
     <header>
-      <div className="container">
+      <nav className="container">
         {/* <Link to="/" className="title">Yoobee College</Link> */}
         <Link to="/">
           <h1>Yoobee College</h1>
         </Link>
 
-        <nav>
-          <button
-            id="menu"
-            onClick={() => {
-              setMenuOpen(!menuOpen);
-            }}>
-            <i className="fa-solid fa-bars"></i>
-          </button>
+        <button
+          id="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
 
-          <div className="search-bar-container">
-            <div className="input-wrapper">
-              <div id="search-icon">
-                <i className="fa-solid fa-magnifying-glass"></i>
-                <input placeholder="Search" />
-              </div>
+        <div className="search-bar-container">
+          <div className="input-wrapper">
+            <div id="search-icon">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+            <input placeholder="Search" className="search-input" />
+          </div>
+        </div>
+
+        <div className="user-login">
+          <div className="user-wrapper">
+            <div id="profile-icon">
+              <Link to="/Signup">
+                <h1><i className="fa-solid fa-circle-user spin-hover"></i> Signup</h1>
+              </Link>
             </div>
           </div>
+        </div>
 
-          <ul className={menuOpen ? "open" : ""}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>Students</li>
-            <li>
-              <Link to="/Login">Login</Link>
-            </li>
-            <li>
-              <Link to="/Signup">Sign Up</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+        <ul className={menuOpen ? "open" : ""} style={{display: "none"}}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>Students</li>
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+          <li>
+            <Link to="/Signup">Sign Up</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
