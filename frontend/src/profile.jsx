66ed7c27868c2ae734d52c87fd7033e924fa6af4
ntoni596 <ntoni596@gmail.com>
2023-08-31 13:24:13 +1200

    hopefully this fixes Annika

--- frontend/src/profile.jsx
+++ frontend/src/profile.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import axios from "axios";
 import ProjectForm from "./components/ProjectForm";
 import { useProjectContext } from "./hooks/useProjectContext";
+import ProjectDetails from "./components/ProjectDetails";
 
 const colourOptions = ["#71B548", "#FF9713", "#014399", "#F14E3A", "#EF38FF"];
 
@@ -12,21 +13,18 @@ const getRandomColour = () => {
 
 const Profile = () => {
   const { projects, dispatch } = useProjectContext();
+  const user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        // axios call
         const response = await axios.get("http://localhost:4000/api/projects");
 
-        // check response status is okay (200)
         if (response.status === 200) {
-          console.log(response.data);
           dispatch({ type: "SET_PROJECTS", payload: response.data });
-          // setStudents(response.data);
         }
       } catch (error) {
-        console.error("Error Fetching Projects/Console.Error", error);
+        console.error("Error Fetching Projects:", error);
       }
     };
 
@@ -35,25 +33,18 @@ const Profile = () => {
 
   const projectElements = projects.map((project) => {
     const randomColour = getRandomColour();
-    const user = JSON.parse(localStorage.getItem("user"));
-    const user_id = user.email;
 
-    if (user_id === project.user_id) {
-      return (
-        <div
-          key={project._id}
-          className="grid-item"
-          style={{ backgroundColor: randomColour }}>
-          <img
-            className="projImage"
-            src={project.imageURL}
-            alt="project image"
-          />
-          <h3 className="projTitle">{project.title}</h3>
-          <p className="projDesc">{project.description}</p>
-        </div>
-      );
-    }
+    return (
+      <div
+        key={project._id}
+        className="grid-item"
+        style={{ backgroundColor: randomColour }}>
+        <img className="projImage" src={project.imageURL} alt="project" />
+        <h3 className="projTitle">{project.title}</h3>
+        <p className="projDesc">{project.description}</p>
+        <ProjectDetails project={project} currentUser={user} />
+      </div>
+    );
   });
 
   if (projects === null) {
